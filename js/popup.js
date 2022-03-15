371a266b338801ffeb61f1b8332a16a9888ee6a9
Schumova Maria <schumova.maria@gmail.com>
2022-03-15 15:58:10 +0300

    Отрисуй меня полностью (часть 1): корректировка проверки наличия данных

--- js/popup.js
+++ js/popup.js
@@ -24,22 +24,31 @@ similarCards.forEach(({author, offer}) => {
   const cardElementChildrenArray = Array.from(cardElementChildren);
 
   cardElement.querySelector('.popup__title').textContent = offer.title;
+
   cardElement.querySelector('.popup__text--address').textContent = offer.address;
-  cardElement.querySelector('.popup__text--price').textContent =  `${offer.price} ₽/ночь`;
+
+  cardElement.querySelector('.popup__text--price').textContent =  `${offer.price ? offer.price : 'Данные не найдены.'} ₽/ночь`;
+
   cardElement.querySelector('.popup__type').textContent = TYPES.find((type) => Object.keys(type).includes(offer.type))[offer.type];
-  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms} комнаты для ${offer.guests} гостей`;
-  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`;
+
+  cardElement.querySelector('.popup__text--capacity').textContent = `${offer.rooms ? offer.rooms : 'Данные не найдены.'} комнаты для ${offer.guests ? offer.guests : 'Данные не найдены.'} гостей`;
+
+  cardElement.querySelector('.popup__text--time').textContent = `Заезд после ${offer.checkin ? offer.checkin : 'Данные не найдены.'}, выезд до ${offer.checkout ? offer.checkout : 'Данные не найдены.'}`;
+
   cardElement.querySelector('.popup__features').textContent = offer.features.join(', ');
+
   cardElement.querySelector('.popup__description').textContent = offer.description;
+
   offer.photos.forEach((photo) => {
     const photoElement = cardElementPhotoTemplate.cloneNode();
     photoElement.src = photo;
     cardElementPhotoContainer.append(photoElement);
   });
+
   cardElement.querySelector('.popup__avatar').src = author.avatar;
 
   cardElementChildrenArray.forEach((child) => {
-    if (child.textContent === 'Данные не найдены.' || child.textContent.includes('undefined') || (!child.textContent && !child.src && child.children.length === 0)) {
+    if (child.textContent.includes('Данные не найдены.') || (!child.textContent && !child.src && child.children.length === 0)) {
       const childClasses = Array.from(child.classList).join('.');
       const notNecessaryChild = cardElement.querySelector(`.${childClasses}`);
       notNecessaryChild.classList.add('hidden');
