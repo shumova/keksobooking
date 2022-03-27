10e6f8c43debf1a294062fc998c5d3b1f3e5126d
Schumova Maria <schumova.maria@gmail.com>
2022-03-27 22:33:06 +0300

    Правда или действие (части 1 и 2)

--- js/form.js
+++ js/form.js
@@ -1,19 +1,100 @@
 const adForm = document.querySelector('.ad-form');
+
+// константы для активации/деактивации страницы
 const mapFilters = document.querySelector('.map__filters');
 const slider = document.querySelector('.ad-form__slider');
 const interactiveElements = Array.from(adForm.children).concat(Array.from(mapFilters.children), slider);
 
-const deactivatePage = () => {
-  adForm.classList.add('ad-form--disabled');
-  mapFilters.classList.add('map__filters--disabled');
-  interactiveElements.forEach((child) => child.setAttribute('disabled', 'disabled'));
+const activatePage = (shouldActivate) => {
+  adForm.classList[shouldActivate ? 'remove' : 'add']('ad-form--disabled');
+  mapFilters.classList[shouldActivate ? 'remove' : 'add']('ad-form--disabled');
+  interactiveElements.forEach((child) => child[shouldActivate ? 'removeAttribute' : 'setAttribute']('disabled', 'disabled'));
 };
 
-const activatePage = () => {
-  adForm.classList.remove('ad-form--disabled');
-  mapFilters.classList.remove('map__filters--disabled');
-  interactiveElements.forEach((child) => child.removeAttribute('disabled'));
+// константы и функции для валидации
+
+const pristine = new Pristine(adForm, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextClass: 'ad-form__error'
+}, false);
+
+//для валидации цены
+
+const price = adForm.querySelector('#price');
+const minPrice = {
+  'bungalow': 0,
+  'flat': 1000,
+  'hotel': 3000,
+  'house': 5000,
+  'palace': 10000
 };
 
-deactivatePage();
-activatePage();
+function validatePrice (value) {
+  const unit = adForm.querySelector('[name="type"]');
+  return value.length && value >= minPrice[unit.value];
+}
+
+function getPriceErrorMessage () {
+  const unit = adForm.querySelector('[name="type"]');
+  return `Минимальная цена, руб.: ${minPrice[unit.value]}`;
+}
+
+function onTypeChange () {
+  price.placeholder = minPrice[this.value];
+  pristine.validate(price);
+}
+
+adForm
+  .querySelectorAll('[name="type"]')
+  .forEach((item) => item.addEventListener('change', onTypeChange));
+
+pristine.addValidator(price, validatePrice,getPriceErrorMessage);
+
+//для валидации комнат-вместимости
+
+const roomsField = adForm.querySelector('[name="rooms"]');
+const capacityField = adForm.querySelector('[name="capacity"]');
+const roomsOption = {
+  '1': ['1'],
+  '2': ['1', '2'],
+  '3': ['1', '2', '3'],
+  '100': ['100']
+};
+
+function validateRooms () {
+  return roomsOption[roomsField.value].includes(capacityField.value);
+}
+
+function getRoomsErrorMessage () {
+  return `
+  Максимальная вместимость, человек:
+  ${roomsField.value === '100' ? 'не для гостей' : roomsField.value}
+  `;
+}
+
+pristine.addValidator(roomsField, validateRooms);
+pristine.addValidator(capacityField, validateRooms, getRoomsErrorMessage);
+
+//для проверки времени заезда-выезда
+const timeInField = adForm.querySelector('#timein');
+const timeOutField = adForm.querySelector('#timeout');
+
+function onTimeChange() {
+  const dependentField = this.id === 'timein' ? timeOutField : timeInField;
+  dependentField.value = this.value;
+}
+
+timeInField.addEventListener('change', onTimeChange);
+timeOutField.addEventListener('change', onTimeChange);
+
+// активация страницы
+activatePage(false);
+activatePage(true);
+
+// валидация формы
+adForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
