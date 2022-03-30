1ae6f5456d42671fdf11aa0aff9629aecdc996d0
Schumova Maria <schumova.maria@gmail.com>
2022-03-30 18:35:53 +0300

    Помощь друга

--- js/form.js
+++ js/form.js
@@ -1,9 +1,14 @@
-const adForm = document.querySelector('.ad-form');
+import {similarCards} from './mock.js';
+import {addContent} from './utils.js';
 
-// константы для активации/деактивации страницы
+const adForm = document.querySelector('.ad-form');
+const resetButton = document.querySelector('.ad-form__reset');
+
+// данные для активации страницы
+
 const mapFilters = document.querySelector('.map__filters');
-const slider = document.querySelector('.ad-form__slider');
-const interactiveElements = Array.from(adForm.children).concat(Array.from(mapFilters.children), slider);
+const sliderElement = document.querySelector('.ad-form__slider');
+const interactiveElements = Array.from(adForm.children).concat(Array.from(mapFilters.children), sliderElement);
 
 const activatePage = (shouldActivate) => {
   adForm.classList[shouldActivate ? 'remove' : 'add']('ad-form--disabled');
@@ -11,25 +16,173 @@ const activatePage = (shouldActivate) => {
   interactiveElements.forEach((child) => child[shouldActivate ? 'removeAttribute' : 'setAttribute']('disabled', 'disabled'));
 };
 
-// константы и функции для валидации
+activatePage(false);
+
+//инициализация карты
+
+const map = L.map('map-canvas')
+  .on('load', () => {
+    activatePage(true);
+  })
+  .setView({
+    lat: 35.652832,
+    lng: 139.839478,
+  }, 12);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+// создание главной метки
+
+const mainPinIcon = L.icon({
+  iconUrl: './img/main-pin.svg',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+
+const mainPinMarker = L.marker(
+  {
+    lat: 35.652832,
+    lng: 139.839478,
+  },
+  {
+    draggable: true,
+    icon: mainPinIcon,
+  }
+);
+
+mainPinMarker.addTo(map);
+
+// создание обычных меток
+
+const icon = L.icon({
+  iconUrl: './img/pin.svg',
+  iconSize: [40, 40],
+  iconAnchor: [20, 40],
+});
+
+// данные для рендера карточек
+
+const RoomsType = {
+  FLAT: 'Квартира',
+  BUNGALOW: 'Бунгало',
+  HOUSE: 'Дом',
+  PALACE: 'Дворец',
+  HOTEL: 'Отель'
+};
+
+const renderCard = ({author, offer}) => {
+
+  const cardTemplate = document.querySelector('#card').content.querySelector('.popup');
+  const cardElement = cardTemplate.cloneNode(true);
+
+  cardElement.querySelector('.popup__avatar').src = author.avatar;
+  cardElement.querySelector('.popup__title').textContent = offer.title;
+  cardElement.querySelector('.popup__text--address').textContent = offer.address;
+  cardElement.querySelector('.popup__text--price').insertAdjacentHTML('afterbegin', `${offer.price} `);
+
+  addContent(cardElement.querySelector('.popup__type'), RoomsType[offer.type.toUpperCase()]);
+  addContent(cardElement.querySelector('.popup__description'), offer.description);
+  addContent(cardElement.querySelector('.popup__text--capacity'), `${offer.rooms} комнаты для ${offer.guests} гостей`);
+  addContent(cardElement.querySelector('.popup__text--time'), `Заезд после ${offer.checkin}, выезд до ${offer.checkout}`);
+
+  const featuresList = cardElement.querySelector('.popup__features');
+  featuresList.textContent = '';
+  offer.features.map((item) => {
+    const featureItem = document.createElement('li');
+    featureItem.classList.add('popup__feature', `popup__feature--${item}`);
+    featureItem.textContent = item;
+    featuresList.appendChild(featureItem);
+  });
+
+  const gallery = cardElement.querySelector('.popup__photos');
+  offer.photos.forEach((item) => {
+    if (item) {
+      const cardPhoto = document.createElement('img');
+      cardPhoto.classList.add('popup__photo');
+      cardPhoto.src = item;
+      cardPhoto.width = 45;
+      cardPhoto.height = 40;
+      cardPhoto.alt = 'Фотография жилья';
+      gallery.appendChild(cardPhoto);
+    } else {
+      gallery.remove();
+    }
+  });
+
+  return cardElement;
+};
+
+// создание слоя с метками
+
+const markerGroup = L.layerGroup().addTo(map);
+
+const createMarker = (card) => {
+  const lat = card.offer.location.lat;
+  const lng = card.offer.location.lng;
+  const marker = L.marker(
+    {
+      lat,
+      lng,
+    },
+    {
+      icon,
+    },
+  );
+
+  marker
+    .addTo(markerGroup)
+    .bindPopup(renderCard(card));
+};
+
+similarCards.forEach((card) => {
+  createMarker(card);
+});
+
+// возвращение маркера в исходное положение
+
+resetButton.addEventListener('click', (evt) => {
+  evt.preventDefault();
+
+  markerGroup.clearLayers();
+
+  mainPinMarker.setLatLng({
+    lat: 35.652832,
+    lng: 139.839478,
+  });
+  map.setView({
+    lat: 35.652832,
+    lng: 139.839478,
+  }, 12);
+});
+
+// получение адреса текущего pin
+
+const getCurrentAddress = (pin) => Object.values(pin.getLatLng()).map((item) => item.toFixed(5)).join(', ');
+const addressField = adForm.querySelector('#address');
+
+addressField.value = getCurrentAddress(mainPinMarker);
+
+mainPinMarker.on('moveend', (evt) => {
+  const currentAddress = evt.target;
+  addressField.value = getCurrentAddress(currentAddress);
+});
+
+// инициализация валидатора
 
 const pristine = new Pristine(adForm, {
   classTo: 'ad-form__element',
   errorTextParent: 'ad-form__element',
   errorTextClass: 'ad-form__error'
-}, false);
+});
 
-const onInputChange = (evt) => {
-  pristine.validate(evt.target);
-};
+// данные для валидации цены
 
-//для валидации заголовка
-
-const title = adForm.querySelector('#title');
-
-//для валидации цены
-
-const price = adForm.querySelector('#price');
+const priceField = adForm.querySelector('#price');
 const MinPrice = {
   BUNGALOW: 0,
   FLAT: 1000,
@@ -49,17 +202,46 @@ const getPriceErrorMessage = () => {
 };
 
 const onTypeChange = (evt) => {
-  price.placeholder = MinPrice[evt.target.value.toUpperCase()];
-  pristine.validate(price);
+  priceField.placeholder = MinPrice[evt.target.value.toUpperCase()];
+  pristine.validate(priceField);
 };
 
 adForm
   .querySelectorAll('[name="type"]')
   .forEach((item) => item.addEventListener('change', onTypeChange));
 
-pristine.addValidator(price, validatePrice,getPriceErrorMessage);
+pristine.addValidator(priceField, validatePrice, getPriceErrorMessage);
 
-//для валидации комнат-вместимости
+// инициализация слайдера для цены и его валидация
+
+priceField.value = 1000;
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100000,
+  },
+  start: 1000,
+  step: 1,
+  connect: 'lower',
+
+  format: {
+    to: function (value) {
+      return value.toFixed(0);
+    },
+    from: function (value) {
+      return parseFloat(value);
+    },
+  },
+
+});
+
+sliderElement.noUiSlider.on('update', () => {
+  priceField.value = sliderElement.noUiSlider.get();
+  pristine.validate(priceField);
+});
+
+// данные для валидации комнат-вместимости
 
 const roomsField = adForm.querySelector('#room_number');
 const capacityField = adForm.querySelector('#capacity');
@@ -67,12 +249,20 @@ const roomsOption = {
   '1': ['1'],
   '2': ['1', '2'],
   '3': ['1', '2', '3'],
-  '100': ['100']
+  '100': ['0']
 };
 
 const onRoomsChange = () => {
   pristine.validate(capacityField);
 };
+
+adForm
+  .querySelectorAll('[name="rooms"]')
+  .forEach((item) => item.addEventListener('change', onRoomsChange));
+
+adForm
+  .querySelectorAll('[name="capacity"]')
+  .forEach((item) => item.addEventListener('change', onRoomsChange));
 
 const validateRooms = () => roomsOption[roomsField.value].includes(capacityField.value);
 
@@ -82,6 +272,7 @@ pristine.addValidator(roomsField, validateRooms);
 pristine.addValidator(capacityField, validateRooms, getRoomsErrorMessage);
 
 //для проверки времени заезда-выезда
+
 const timeInField = adForm.querySelector('#timein');
 const timeOutField = adForm.querySelector('#timeout');
 
@@ -93,21 +284,8 @@ const onTimeChange = (evt) => {
 timeInField.addEventListener('change', onTimeChange);
 timeOutField.addEventListener('change', onTimeChange);
 
-// активация страницы
-activatePage(false);
-activatePage(true);
-
 // валидация формы
 adForm.addEventListener('submit', (evt) => {
   evt.preventDefault();
   pristine.validate();
-
-  title.addEventListener('keyup', onInputChange);
-  price.addEventListener('keyup', onInputChange);
-  adForm
-    .querySelectorAll('[name="rooms"]')
-    .forEach((item) => item.addEventListener('change', onRoomsChange));
-  adForm
-    .querySelectorAll('[name="capacity"]')
-    .forEach((item) => item.addEventListener('change', onRoomsChange));
 });
--- js/popup.js
+++
@@ -1,9 +0,0 @@
-const RoomsType = {
-  FLAT: 'Квартира',
-  BUNGALOW: 'Бунгало',
-  HOUSE: 'Дом',
-  PALACE: 'Дворец',
-  HOTEL: 'Отель'
-};
-
-export {RoomsType};
