f40cd835f2b9cc4565ededca134566c11ce6288f
Schumova Maria <schumova.maria@gmail.com>
2022-04-11 15:27:09 +0300

    Исправление функции добавления фото

--- js/photos.js
+++ js/photos.js
@@ -1,9 +1,9 @@
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+const MAX_PHOTO_QUANTITY = 6;
 const avatarChooser = document.querySelector('.avatar__input');
 const avatarPreview = document.querySelector('.avatar__photo');
 const offerPhotosChooser = document.querySelector('.ad-form__input');
 const offerPhotosPreview = document.querySelector('.ad-form__photo');
-const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-const MAX_PHOTO_QUANTITY = 6;
 
 let currentQuantity = 0;
 const resetPhotos = () => {
@@ -15,11 +15,13 @@ const resetPhotos = () => {
 
 avatarChooser.addEventListener('change', () => {
   const file = avatarChooser.files[0];
-  const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  if (file) {
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
-  if (matches) {
-    avatarPreview.src = URL.createObjectURL(file);
+    if (matches) {
+      avatarPreview.src = URL.createObjectURL(file);
+    }
   }
 });
 
@@ -27,14 +29,19 @@ offerPhotosChooser.addEventListener('change', () => {
   const files = Array.from(offerPhotosChooser.files);
   files.forEach((file) => {
     currentQuantity++;
-    if (currentQuantity <= MAX_PHOTO_QUANTITY) {
-      const photoElement = document.createElement('img');
-      photoElement.src = URL.createObjectURL(file);
-      photoElement.alt = 'Фотография объекта';
-      photoElement.width = 70;
-      photoElement.height = 70;
-      photoElement.classList.add('ad-form__photo-item');
-      offerPhotosPreview.insertAdjacentElement('beforeend', photoElement);
+    const fileName = file.name.toLowerCase();
+    const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+
+    if(matches) {
+      if (currentQuantity <= MAX_PHOTO_QUANTITY) {
+        const photoElement = document.createElement('img');
+        photoElement.src = URL.createObjectURL(file);
+        photoElement.alt = 'Фотография объекта';
+        photoElement.width = 70;
+        photoElement.height = 70;
+        photoElement.classList.add('ad-form__photo-item');
+        offerPhotosPreview.insertAdjacentElement('beforeend', photoElement);
+      }
     }
 
     if (currentQuantity === MAX_PHOTO_QUANTITY) {
@@ -48,6 +55,7 @@ offerPhotosChooser.addEventListener('change', () => {
       }
     }
   });
+  offerPhotosChooser.value = '';
 });
 
 export {resetPhotos};
